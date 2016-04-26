ce213c30a04032de40dcb99f7d3ff16a2ddb9548
Luca Baldini <luca.baldini@pi.infn.it>
2016-04-26 14:49:49 +0200

    Global install function added.

--- quickusb-2.15.2/linux/setup.sh
+++ quickusb-2.15.2/linux/setup.sh
@@ -8,7 +8,7 @@ qusb_install_kernel_module()
     KM_INSTALL_PATH=/lib/modules/$(uname -r)/kernel/drivers/usb/misc
     if [ -f $KM_INSTALL_PATH/qusb_lnx.ko ];
     then
-	:
+	echo "Kernel module found, nothing to do."
     else
 	echo "Compiling kernel..."
 	cd driver
@@ -24,7 +24,7 @@ qusb_link_libs()
     # Create the links to the libraries if they don't exist.
     if [ -f lib/$ARCH/libquickusb.so ];
     then
-	:
+	echo "Lib links found, nothing to do."
     else
 	cd lib/$ARCH/
 	echo "Creating soft link in" $(pwd) "..."
@@ -38,21 +38,29 @@ qusb_copy_rule_file()
     # Copy the rule file.
     if [ -f /etc/udev/rules.d/90-qusb.rules ];
     then
-	:
+	echo "Rule file found, nothing to do."
     else
 	echo "Copying 90-qusb.rules to /etc/udev/rules.d/..."
 	sudo cp 90-qusb.rules /etc/udev/rules.d/
     fi
 }
 
-qusb_copy_usb_scripts()
+qusb_copy_usb_script()
 {
     # Copy the USB script.
     if [ -f /usr/local/bin/qusb_script.sh ];
     then
-	:
+	echo "USB script found, nothing to do."
     else
 	echo "Copying qusb_script.sh in /usr/local/bin..."
 	sudo cp qusb_script.sh /usr/local/bin
     fi
+}
+
+qusb_install()
+{
+    qusb_install_kernel_module
+    qusb_link_libs
+    qusb_copy_rule_file
+    qusb_copy_usb_script
 }
