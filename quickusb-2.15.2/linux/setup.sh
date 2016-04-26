b04e235eb6e550d7f84772c977e6b39edf4af323
Luca Baldini <luca.baldini@pi.infn.it>
2016-04-26 13:33:57 +0200

    Linux setup script refactored in functions.

--- quickusb-2.15.2/linux/setup.sh
+++ quickusb-2.15.2/linux/setup.sh
@@ -2,44 +2,57 @@
 QUICKUSB_VERSION=2.15.2
 ARCH=$(uname -m)
 
-# Compile the driver.
-KM_INSTALL_PATH=/lib/modules/$(uname -r)/kernel/drivers/usb/misc
-if [[ -f $KM_INSTALL_PATH/qusb_lnx.ko ]];
-then
-    :
-else
-    echo "Compiling kernel..."
-    cd driver
-    make
-    echo "Copying kernel module..."
-    sudo cp $(uname -m)/qusb_lnx.ko $KM_INSTALL_PATH
-    cd -
-fi
+qusb_install_kernel_module()
+{
+    # Compile the driver.
+    KM_INSTALL_PATH=/lib/modules/$(uname -r)/kernel/drivers/usb/misc
+    if [ -f $KM_INSTALL_PATH/qusb_lnx.ko ];
+    then
+	:
+    else
+	echo "Compiling kernel..."
+	cd driver
+	make
+	echo "Copying kernel module..."
+	sudo cp $(uname -m)/qusb_lnx.ko $KM_INSTALL_PATH
+	cd -
+    fi
+}
 
-# Create the links to the libraries if they don't exist.
-if [[ -f lib/$ARCH/libquickusb.so ]];
-then
-    :
-else
-    cd lib/$ARCH/
-    echo "Creating soft link in" $(pwd) "..."
-    ln -s libquickusb.so.$QUICKUSB_VERSION libquickusb.so
-fi
+qusb_link_libs()
+{
+    # Create the links to the libraries if they don't exist.
+    if [ -f lib/$ARCH/libquickusb.so ];
+    then
+	:
+    else
+	cd lib/$ARCH/
+	echo "Creating soft link in" $(pwd) "..."
+	ln -s libquickusb.so.$QUICKUSB_VERSION libquickusb.so
+	cd -
+    fi
+}
 
-# Copy the rule file.
-if [[ -f /etc/udev/rules.d/90-qusb.rules ]];
-then
-    :
-else
-    echo "Copying 90-qusb.rules to /etc/udev/rules.d/..."
-    sudo cp 90-qusb.rules /etc/udev/rules.d/
-fi
+qusb_copy_rule_file()
+{
+    # Copy the rule file.
+    if [ -f /etc/udev/rules.d/90-qusb.rules ];
+    then
+	:
+    else
+	echo "Copying 90-qusb.rules to /etc/udev/rules.d/..."
+	sudo cp 90-qusb.rules /etc/udev/rules.d/
+    fi
+}
 
-# Copy the USB script.
-if [[ -f /usr/local/bin/qusb_script.sh ]];
-then
-    :
-else
-    echo "Copying qusb_script.sh in /usr/local/bin..."
-    sudo cp qusb_script.sh /usr/local/bin
-fi
+qusb_copy_usb_scripts()
+{
+    # Copy the USB script.
+    if [ -f /usr/local/bin/qusb_script.sh ];
+    then
+	:
+    else
+	echo "Copying qusb_script.sh in /usr/local/bin..."
+	sudo cp qusb_script.sh /usr/local/bin
+    fi
+}
