caf388c018c3f95323022ff7e4267f126b25ad24
Luca Baldini <luca.baldini@pi.infn.it>
2016-04-27 06:09:05 +0200

    First attempt at a release manager.

--- __version__.h
+++ __version__.h
@@ -1,2 +1,2 @@
-#define __XPEDAQ_VERSION__ "0.0.7"
-#define __XPEDAQ_BUILD_DATE__ "Tue, 26 Apr 2016 22:56:09 +0200"
+#define __XPEDAQ_VERSION__ "0.0.10"
+#define __XPEDAQ_BUILD_DATE__ "Wed, 27 Apr 2016 06:06:02 +0200"
--- release/release.py
+++ release/release.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
+# Copyright (C) 2007--2016 the X-ray Polarimetry Explorer (XPE) team.
+#
+# For the license terms see the file LICENSE, distributed along with this
+# software.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-"""
-Copyright (C) 2007--2016 the X-ray Polarimetry Explorer (XPE) team.
-
-For the license terms see the file LICENSE, distributed along with this
-software.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 2 of the License, or (at your
-option) any later version.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-"""
 
 import os
 import time
@@ -27,16 +25,16 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
+PACKAGE_NAME = 'xpedaq'
 XPEDAQ_ROOT = os.environ['XPOL_DAQ_ROOT']
-
 VERSION_FILE_PATH = os.path.join(XPEDAQ_ROOT, '__version__.h')
+RELEASE_NOTES_FILE_PATH = os.path.join(XPEDAQ_ROOT, 'doc', 'release.notes')
 BUILD_DATE = time.strftime('%a, %d %b %Y %H:%M:%S %z')
-
 TAG_MODES = ['major', 'minor', 'patch']
 
 
 def cmd(command):
-    """
+    """Execute a system command.
     """
     logging.info('About to execute "%s"...' % command)
     os.system(command)
@@ -73,19 +71,36 @@ def updateVersionFile(mode):
     logging.info('Done.')
     return nextTag
 
+def updateReleaseNotes(tag):
+    """ Write the new tag and build date on top of the release notes.
+    """
+    hline = '-'*79
+    logging.info('Reading in %s...' % RELEASE_NOTES_FILE_PATH)
+    notes = open(RELEASE_NOTES_FILE_PATH).read().strip('\n')
+    logging.info('Writing out %s...' % RELEASE_NOTES_FILE_PATH)
+    info = '\n%s\n%s (%s) - %s\n%s\n' %\
+           (hline, PACKAGE_NAME, tag, BUILD_DATE, hline)
+    outputFile = open(RELEASE_NOTES_FILE_PATH, 'w')
+    outputFile.writelines(info)
+    outputFile.writelines(notes)
+    outputFile.close()
+    logging.info('Done.')
+
 def tag(mode):
     """ Tag the package.
     """
     cmd('git pull')
     cmd('git status')
     tag = updateVersionFile(mode)
-    #updateReleaseNotes(tag, dryRun)
+    updateReleaseNotes(tag)
     msg = 'Prepare for tag %s.' % tag
     cmd('git commit -a -m "%s"' % msg)
-    #cmd('git push')
-    #__utils__.cmd('hg tag %s' % tag, verbose = True, dryRun = dryRun)
-    #__utils__.cmd('hg push', verbose = True, dryRun = dryRun)
-    #__utils__.cmd('hg status', verbose = True, dryRun = dryRun)
+    cmd('git push')
+    cmd('hg tag %s' % tag, verbose = True, dryRun = dryRun)
+    msg = 'tagging version %s' % tag
+    cmd('git tag -a %s -m "%s"' % (tag, msg))
+    cmd('git push --tags')
+    cmd('git status')
     #compilePackage()
     return tag
 
