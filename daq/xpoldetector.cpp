65c8a175b1bbe200ad307ee5c9f786aa8948fece
Luca Baldini <luca.baldini@pi.infn.it>
2016-05-07 06:27:43 +0200

    Minor changes.

--- daq/xpoldetector.cpp
+++ daq/xpoldetector.cpp
@@ -22,24 +22,24 @@ with this program; if not, write to the Free Software Foundation Inc.,
 
 #include "xpoldetector.h"
 
-const short unsigned int xpoldetector::kUndefinedReadoutCode = 0x0;
-const short unsigned int xpoldetector::kFullFrameReadoutCode = 0x1;
-const short unsigned int xpoldetector::kWindowedReadoutCode = 0x2;
-const short unsigned int xpoldetector::kChargeInjectionReadoutCode = 0x4;
-const short unsigned int xpoldetector::kThresholdDacNumBits = 12;
-const short unsigned int xpoldetector::kThresholdDacMin = 0;
-const short unsigned int xpoldetector::kThresholdDacMax =
-(short unsigned int)(pow(2, xpoldetector::kThresholdDacNumBits) - 1);
+const unsigned short xpoldetector::kUndefinedReadoutCode = 0x0;
+const unsigned short xpoldetector::kFullFrameReadoutCode = 0x1;
+const unsigned short xpoldetector::kWindowedReadoutCode = 0x2;
+const unsigned short xpoldetector::kChargeInjectionReadoutCode = 0x4;
+const unsigned short xpoldetector::kThresholdDacNumBits = 12;
+const unsigned short xpoldetector::kThresholdDacMin = 0;
+const unsigned short xpoldetector::kThresholdDacMax =
+(unsigned short)(pow(2, xpoldetector::kThresholdDacNumBits) - 1);
 const int xpoldetector::kThresholdVoltageMin = 0;
 const int xpoldetector::kThresholdVoltageMax =
 xpoldetector::thrDacToVoltage(xpoldetector::kThresholdDacMax - 1);
 const int xpoldetector::kThresholdChargeMin = 0;
 const int xpoldetector::kThresholdChargeMax =
 xpoldetector::thrDacToCharge(xpoldetector::kThresholdDacMax - 1);
-const short unsigned int xpoldetector::kCalibrationDacNumBits = 12;
-const short unsigned int xpoldetector::kCalibrationDacMin = 0;
-const short unsigned int xpoldetector::kCalibrationDacMax =
-(short unsigned int)(pow(2, xpoldetector::kCalibrationDacNumBits) - 1);
+const unsigned short xpoldetector::kCalibrationDacNumBits = 12;
+const unsigned short xpoldetector::kCalibrationDacMin = 0;
+const unsigned short xpoldetector::kCalibrationDacMax =
+(unsigned short)(pow(2, xpoldetector::kCalibrationDacNumBits) - 1);
 const int xpoldetector::kCalibrationVoltageMin = 0;
 const int xpoldetector::kCalibrationVoltageMax =
 xpoldetector::calDacToVoltage(xpoldetector::kCalibrationDacMax);
@@ -53,108 +53,108 @@ const double xpoldetector::kThresholdSensitivity = 200.0;
 const double xpoldetector::kCalibrationSensitivity = 0.010;
 const double xpoldetector::kElectronsPerfC = 6200.0;
 
-int xpoldetector::thrDacToVoltage(short unsigned int dac)
+int xpoldetector::thrDacToVoltage(unsigned short dac)
 {
   double voltage = dac*kThresholdDacTomV;
   return (int)(0.5 + voltage);
 }
 
-int xpoldetector::thrDacToCharge(short unsigned int dac)
+int xpoldetector::thrDacToCharge(unsigned short dac)
 {
   double charge = dac*kThresholdDacTomV*kElectronsPerfC/kThresholdSensitivity;
   return (int)(0.5 + charge);
 }
 
-short unsigned int xpoldetector::thrVoltageToDac(int voltage)
+unsigned short xpoldetector::thrVoltageToDac(int voltage)
 {
   double dac = voltage/kThresholdDacTomV;
-  return (short unsigned int)(0.5 + dac);
+  return (unsigned short)(0.5 + dac);
 }
 
-short unsigned int xpoldetector::thrChargeToDac(int charge)
+unsigned short xpoldetector::thrChargeToDac(int charge)
 {
   double dac = charge*kThresholdSensitivity/kThresholdDacTomV/kElectronsPerfC;
-  return (short unsigned int)(0.5 + dac);
+  return (unsigned short)(0.5 + dac);
 }
 
-int xpoldetector::refDacToVoltage(short unsigned int dac)
+int xpoldetector::refDacToVoltage(unsigned short dac)
 {
   double voltage = dac*kReferenceDacTomV;
   return (int)(0.5 + voltage);
 }
 
-int xpoldetector::refDacToCharge(short unsigned int dac)
+int xpoldetector::refDacToCharge(unsigned short dac)
 {
   double charge = dac*kReferenceDacTomV*kElectronsPerfC/kThresholdSensitivity;
   return (int)(0.5 + charge);
 }
 
-short unsigned int xpoldetector::refVoltageToDac(int voltage)
+unsigned short xpoldetector::refVoltageToDac(int voltage)
 {
   double dac = voltage/kReferenceDacTomV;
-  return (short unsigned int)(0.5 + dac);
+  return (unsigned short)(0.5 + dac);
 }
 
-short unsigned int xpoldetector::refChargeToDac(int charge)
+unsigned short xpoldetector::refChargeToDac(int charge)
 {
   double dac = charge*kThresholdSensitivity/kReferenceDacTomV/kElectronsPerfC;
-  return (short unsigned int)(0.5 + dac);
+  return (unsigned short)(0.5 + dac);
 }
 
-int xpoldetector::calDacToVoltage(short unsigned int dac)
+int xpoldetector::calDacToVoltage(unsigned short dac)
 {
   double voltage = dac*kCalibrationDacTomV;
   return (int)(0.5 + voltage);
 }
 
-int xpoldetector::calDacToCharge(short unsigned int dac)
+int xpoldetector::calDacToCharge(unsigned short dac)
 {
   double charge = dac*kCalibrationDacTomV*kElectronsPerfC*
     kCalibrationSensitivity;
   return (int)(0.5 + charge);
 }
 
-short unsigned int xpoldetector::calChargeToDac(int charge)
+unsigned short xpoldetector::calChargeToDac(int charge)
 {
   double dac = charge/(kCalibrationSensitivity*kCalibrationDacTomV*
 		       kElectronsPerfC);
-  return (short unsigned int)(0.5 + dac);
+  return (unsigned short)(0.5 + dac);
 }
 
-short unsigned int xpoldetector::calVoltageToDac(int voltage)
+unsigned short xpoldetector::calVoltageToDac(int voltage)
 {
   double dac = voltage/kCalibrationDacTomV;
-  return (short unsigned int)(0.5 + dac);
+  return (unsigned short)(0.5 + dac);
 }
 
-unsigned short int xpoldetector::kClockShiftMin = 0;
-unsigned short int xpoldetector::kClockShiftMax = 800;
-unsigned short int xpoldetector::kClockShiftStep = 25;
-unsigned short int xpoldetector::kSubSamplesZero = 0;
-unsigned short int xpoldetector::kSubSamplesSmaller = 1;
-unsigned short int xpoldetector::kSubSamplesSmall = 2;
-unsigned short int xpoldetector::kSubSamplesLarge = 4;
-unsigned short int xpoldetector::kSubSamplesLarger = 8;
-unsigned short int xpoldetector::kPedSubDelayMin = 100;
-unsigned short int xpoldetector::kPedSubDelayMax = 1000;
-unsigned short int xpoldetector::kPedSubDelayStep = 50;
-unsigned short int xpoldetector::kTrgEnableDelayMin = 50;
-unsigned short int xpoldetector::kTrgEnableDelayMax = 500;
-unsigned short int xpoldetector::kTrgEnableDelayStep = 50;
-unsigned short int xpoldetector::kMinWindowSizeMin = 32;
-unsigned short int xpoldetector::kMinWindowSizeMax = 1024;
-unsigned short int xpoldetector::kMinWindowSizeStep = 32;
-unsigned short int xpoldetector::kMaxWindowSizeMin = 512;
-unsigned short int xpoldetector::kMaxWindowSizeMax = 5120;
-unsigned short int xpoldetector::kMaxWindowSizeStep = 512;
-unsigned short int xpoldetector::kClockFasterFreqCode = 0x0;
-unsigned short int xpoldetector::kClockFastFreqCode = 0x20;
-unsigned short int xpoldetector::kClockSlowFreqCode = 0x40;
-unsigned short int xpoldetector::kClockSlowerFreqCode = 0x60;
-unsigned short int xpoldetector::kNumPixelsX = 352;
-unsigned short int xpoldetector::kNumPixelsY = 300;
-unsigned short int xpoldetector::kSmallBufferMode = 0x1;
-unsigned short int xpoldetector::kLargeBufferMode = ~xpoldetector::kSmallBufferMode;
+const unsigned short xpoldetector::kClockShiftMin = 0;
+const unsigned short xpoldetector::kClockShiftMax = 800;
+const unsigned short xpoldetector::kClockShiftStep = 25;
+const unsigned short xpoldetector::kSubSamplesZero = 0;
+const unsigned short xpoldetector::kSubSamplesSmaller = 1;
+const unsigned short xpoldetector::kSubSamplesSmall = 2;
+const unsigned short xpoldetector::kSubSamplesLarge = 4;
+const unsigned short xpoldetector::kSubSamplesLarger = 8;
+const unsigned short xpoldetector::kPedSubDelayMin = 100;
+const unsigned short xpoldetector::kPedSubDelayMax = 1000;
+const unsigned short xpoldetector::kPedSubDelayStep = 50;
+const unsigned short xpoldetector::kTrgEnableDelayMin = 50;
+const unsigned short xpoldetector::kTrgEnableDelayMax = 500;
+const unsigned short xpoldetector::kTrgEnableDelayStep = 50;
+const unsigned short xpoldetector::kMinWindowSizeMin = 32;
+const unsigned short xpoldetector::kMinWindowSizeMax = 1024;
+const unsigned short xpoldetector::kMinWindowSizeStep = 32;
+const unsigned short xpoldetector::kMaxWindowSizeMin = 512;
+const unsigned short xpoldetector::kMaxWindowSizeMax = 5120;
+const unsigned short xpoldetector::kMaxWindowSizeStep = 512;
+const unsigned short xpoldetector::kClockFasterFreqCode = 0x0;
+const unsigned short xpoldetector::kClockFastFreqCode = 0x20;
+const unsigned short xpoldetector::kClockSlowFreqCode = 0x40;
+const unsigned short xpoldetector::kClockSlowerFreqCode = 0x60;
+const unsigned short xpoldetector::kNumPixelsX = 352;
+const unsigned short xpoldetector::kNumPixelsY = 300;
+const unsigned short xpoldetector::kSmallBufferMode = 0x1;
+const unsigned short xpoldetector::kLargeBufferMode = ~xpoldetector::kSmallBufferMode;
 
-int xpoldetector::kSmallBufferSize = 10000;
-int xpoldetector::kLargeBufferSize = 2*262144;
+const int xpoldetector::kSmallBufferSize = 2*5000;
+const int xpoldetector::kLargeBufferSize = 2*262144;
