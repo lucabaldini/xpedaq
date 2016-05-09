3e7646deeecb86cde6bd6a880991b390661d6613
Luca Baldini <luca.baldini@pi.infn.it>
2016-05-09 12:30:33 +0200

    Prepare for tag 0.8.2.

--- __version__.h
+++ __version__.h
@@ -1,2 +1,2 @@
-#define __XPEDAQ_VERSION__ "0.8.1"
-#define __XPEDAQ_BUILD_DATE__ "Mon, 09 May 2016 06:17:37 +0200"
+#define __XPEDAQ_VERSION__ "0.8.2"
+#define __XPEDAQ_BUILD_DATE__ "Mon, 09 May 2016 12:30:31 +0200"
