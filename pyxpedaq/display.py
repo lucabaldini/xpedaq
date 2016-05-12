0265aa21638f4a38280ea15d2d0d8fa55105ab8b
Luca Baldini <luca.baldini@pi.infn.it>
2016-05-12 11:10:09 +0200

    Minor.

--- pyxpedaq/display.py
+++ pyxpedaq/display.py
@@ -50,28 +50,49 @@ class pHexagonalMatrix():
         return self.num_columns*self.num_pixels
 
     def pixel2world(self, col, row):
+        """Convert from pixel coordinates to world coordinates.
+
+        This is using the DAQ coordinate system, where the origin is in the
+        top-left corner of the array, the x coordinate is spanning the columns
+        from left to right and the y coordinate is spanning the rows from top
+        to bottom.
         """
-        """
-        x = (col - 0.5*(self.num_columns - 0.5) +\
-             0.5*((row + 1) % 2))*self.COLUMN_PITCH
-        y = (-row + 0.5*(self.num_rows - 1))*self.ROW_PITCH
+        x = (col - 0.5*((row + 1) % 2) + 0.5)*self.COLUMN_PITCH
+        y = -row*self.ROW_PITCH
         return (x, y)
+
+    def frame(self, padding=0.1):
+        """Return a (xmin, ymin, xmax, ymax) containing the entire matrix.
+        """
+        xmin, ymin = self.pixel2world(0, self.num_rows - 1)
+        xmax, ymax = self.pixel2world(self.num_columns - 1, 0)
+        # Make sure we include the pixel edges.
+        xmin -= 0.5*self.COLUMN_PITCH
+        xmax += 0.5*self.COLUMN_PITCH
+        ymin -= 0.5*self.ROW_PITCH
+        ymin += 0.5*self.ROW_PITCH
+        # Add the padding.
+        dx = padding*(xmax - xmin)
+        dy = padding*(ymax - ymin)
+        xmin -= dx
+        xmax += dx
+        ymin -= dy
+        ymax += dy
+        return xmin, ymin, xmax, ymax
 
     def draw(self):
         """
         """
-        xmin, ymin = self.pixel2world(0, self.num_rows - 1)
-        xmax, ymax = self.pixel2world(self.num_columns - 1, 0)
-        lim = 1.2*max(abs(xmin), abs(xmax), max(abs(ymin), abs(ymax)))
+        xmin, ymin, xmax, ymax = self.frame()
         fig = plt.figure(figsize=(10, 10), dpi=80, facecolor='w')
         ax = plt.subplot(111, aspect='equal', adjustable='box-forced')
-        ax.set_xlim([-lim, lim])
-        ax.set_ylim([-lim, lim])
+        ax.set_xlim([xmin, xmax])
+        ax.set_ylim([ymin, ymax])
         # Overall average canvas dimensions in pixels.
-        dim = (ax.transData.transform((lim, lim)) -
-               ax.transData.transform((-lim, -lim))).mean()
+        dim = (ax.transData.transform((xmin, ymin)) -
+               ax.transData.transform((xmax, ymax))).mean()
         # Calculate a something proportional to the hexagon area in px**2. 
-        dim = (dim/(2*lim)*0.85*self.COLUMN_PITCH)**2        
+        dim = (dim/(xmax-xmin)*0.85*self.COLUMN_PITCH)**2        
         poly = collections.RegularPolyCollection(6,
                                                  offsets=self.__grid,
                                                  sizes=(dim,),
