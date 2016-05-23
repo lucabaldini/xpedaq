84e06a7b87c87a6a30208031e6bab3457fc58004
alberto manfreda <albertom@pi.infn.it>
2016-05-23 14:24:14 +0200

    first import

---
+++ daq/pEvent.cpp
@@ -0,0 +1,83 @@
+#include "pEvent.h"
+
+pEvent::pEvent(unsigned int xmin, unsigned int xmax, unsigned int ymin,
+               unsigned int ymax, unsigned int bufferId,
+               event::Adc_vec_ut adcCounts) :
+               m_xmin(xmin), m_xmax(xmax), m_ymin(ymin), m_ymax(ymax),
+               m_bufferId(bufferId), m_adcCounts (adcCounts)
+{
+}
+
+
+std::vector<int> pEvent::pulseHeightsOverThreshold(int threshold)
+{
+  using namespace event;
+  std::vector<int> heightsOT;
+  for(Adc_vec_ut::iterator it = m_adcCounts.begin();
+      it != m_adcCounts.end();
+      ++it)
+   {
+     int height = static_cast<int>(*it);
+     if (height > threshold){heightsOT.push_back(height);}
+   }
+  return heightsOT;
+}
+
+
+unsigned int pEvent::nColumns() const
+{
+  return (m_xmax - m_xmin + 1);
+}
+
+
+unsigned int pEvent::nRows() const
+{
+  return (m_ymax - m_ymin + 1);
+}
+
+
+unsigned int pEvent::totalAdcCounts() const
+{
+  using namespace event;
+  unsigned int sum = 0; 
+  for(Adc_vec_ut::const_iterator it = m_adcCounts.begin();
+      it != m_adcCounts.end();
+      ++it)
+   {
+     sum += (*it);
+   }
+   return sum;
+}
+
+
+void pEvent::barycenter (double &x, double &y)
+{
+  
+  x = 0.;
+  y = 0.;
+  for (unsigned int i = 0; i < nColumns(); i ++)
+  {
+    for (unsigned int j = 0; j < nRows(); j ++)
+    {
+      x += ((m_xmin + i) * m_adcCounts [i + nColumns() * j]);
+      y += ((m_ymin + j) * m_adcCounts [i + nColumns() * j]);
+    }
+  }
+  x /= totalAdcCounts();
+  y /= totalAdcCounts();
+  
+  /* The algorithm above is wrong. For now returning simply the center of
+     the window. */
+  x = 0.5*(m_xmin + m_xmax);
+  y = 0.5*(m_ymin + m_ymax);
+}
+
+
+std::ostream& pEvent::fillStream(std::ostream& os) const
+{
+  os << "w(" << std::setfill(' ') << std::setw(3) << xmin() << ", "
+     << std::setw(3) << ymin() << ")--(" << std::setw(3) << xmax() << ", "
+     << std::setw(3) << ymax() << ") "
+     << std::endl;
+  return os;
+}
---
+++ daq/pEvent.h
@@ -0,0 +1,49 @@
+#ifndef PEVENT_H
+#define PEVENT_H
+
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+namespace event
+{
+  typedef std::vector<unsigned int> Adc_vec_ut;
+}
+
+class pEvent
+{
+  
+  public:
+    pEvent(unsigned int xmin, unsigned int xmax, unsigned int ymin,
+           unsigned int ymax, unsigned int bufferId,
+           event::Adc_vec_ut adcCounts);
+  public:
+    std::vector<int> pulseHeightsOverThreshold (int threshold);
+    void barycenter (double &x, double &y);
+    unsigned int xmin() const {return m_xmin;}
+    unsigned int xmax() const {return m_xmax;}
+    unsigned int ymin() const {return m_ymin;}
+    unsigned int ymax() const {return m_ymax;}
+    unsigned int bufferId() const {return m_bufferId;}
+    unsigned int nColumns() const;
+    unsigned int nRows() const;
+    unsigned int totalAdcCounts() const;
+    
+    // Terminal formatting.
+    std::ostream& fillStream(std::ostream& os) const;
+    friend std::ostream& operator<<(std::ostream& os, const pEvent& event)
+    {return event.fillStream(os);}
+    
+    
+  private:
+    unsigned int m_xmin;
+    unsigned int m_xmax;
+    unsigned int m_ymin;
+    unsigned int m_ymax;
+    unsigned int m_bufferId;
+    event::Adc_vec_ut m_adcCounts;
+    
+};
+
+#endif //PEVENT_H
