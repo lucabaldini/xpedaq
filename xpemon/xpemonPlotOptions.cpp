d5ded1385d570e5c4326af23ed2af58d71bcfe69
alberto manfreda <albertom@pi.infn.it>
2016-06-14 12:12:52 +0200

    first import

---
+++ xpemon/xpemonPlotOptions.cpp
@@ -0,0 +1,12 @@
+#include "xpemonPlotOptions.h"
+
+const unsigned int xpemonPlotOptions::pulseHeightNbins = 100;
+const double xpemonPlotOptions::pulseHeightXmin = 0.;
+const double xpemonPlotOptions::pulseHeightXmax = 10000.;
+const unsigned int xpemonPlotOptions::windowSizeNbins = 100;
+const double xpemonPlotOptions::windowSizeXmin = 0.;
+const double xpemonPlotOptions::windowSizeXmax = 2000.;
+const double xpemonPlotOptions::xPixelMax =
+                               static_cast<double> (xpoldetector::kNumPixelsX);
+const double xpemonPlotOptions::yPixelMax =
+                               static_cast<double> (xpoldetector::kNumPixelsY);
