36d9df8c17ce06a1d859dcb8d7d6e989d2bd163b
alberto manfreda <albertom@pi.infn.it>
2016-06-15 13:04:46 +0200

    Prepare for tag 0.10.0.

--- __version__.h
+++ __version__.h
@@ -1,2 +1,2 @@
-#define __XPEDAQ_VERSION__ "0.9.0"
-#define __XPEDAQ_BUILD_DATE__ "Thu, 12 May 2016 15:24:25 +0200"
+#define __XPEDAQ_VERSION__ "0.10.0"
+#define __XPEDAQ_BUILD_DATE__ "Wed, 15 Jun 2016 13:04:45 +0200"
