87f90e6731f8e5d0d9e7c59b92c9b9ff91198b39
Luca Baldini <luca.baldini@pi.infn.it>
2016-11-27 22:13:22 +0100

    Some doc added.

--- gui/pDataDisplayWidget.h
+++ gui/pDataDisplayWidget.h
@@ -34,6 +34,30 @@ with this program; if not, write to the Free Software Foundation Inc.,
 #include "xpollog.h"
 
 
+/*! Generic interface class to display information on a GUI.
+  
+This is essentially an ensamble of pairs QLabel widgets arranged vertically.
+For each row, the left widget is a simple descriptive text label, while the
+right widget holds the actual data content. Each field is identified by a 
+unique name and, internally, the widgets are mapped into a std::map in order
+to be able to retrieve them.
+
+The two main class method are addField() and set()---the latter in a few
+different incarnations depeding on the data type. This is actually slightly
+less than what we need, as the main use for this class is to connect the
+set() slots to signals. One possibility would be to use a QSignalMapper, but
+we opted instead for a scheme in which addField() is private and all the
+iteraction with the class instance are via a well defined set of predefined
+methods to add and set fields. While this is not terribly elegant (in principle
+there is no guarantee that the key we might want to set exists), it provides
+a flexible scheme to combine fields in an arbitrary fashion and helps ensuring
+consistency across applications.
+
+One more alternative scheme might be to get rid of all the specific add and
+set methods and define exactly what we need in subclasses. We might want to
+think about.
+*/
+
 class pDataDisplayWidget : public QGroupBox
 {
 
@@ -44,6 +68,7 @@ class pDataDisplayWidget : public QGroupBox
     pDataDisplayWidget(const QString &title, QWidget *parent);
     pDataDisplayWidget(QWidget *parent);
 
+    // Predefined field that can be added...
     void addStationId() {addField("station_id", "Station ID");}
     void addRunId() {addField("run_id", "Run Id");}
     void addStatus() {addField("status", "Status");}
@@ -63,6 +88,7 @@ class pDataDisplayWidget : public QGroupBox
     void set(const QString &name, int value);
     void set(const QString &name, double value, int precision);
 
+    // ...and set methods for all the predefined fields;
     void setStationId(int value) {set("station_id", value);}
     void setRunId(int value) {set("run_id", value);}
     void setStatus(QString value) {set("status", value);}
@@ -79,6 +105,8 @@ class pDataDisplayWidget : public QGroupBox
   
     QGridLayout *m_gridLayout;
     std::map<const QString, QLabel*> *m_widgetMap;
+    // This is private to force users to interact with the class via the
+    // public predefined methods.
     void addField(const QString &name, const QString &label);
     QLabel *dataWidget(const QString &name);
 };
