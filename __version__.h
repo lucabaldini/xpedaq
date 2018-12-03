3e7ede2211fdea58a93e15548f7fdbae327bc747
lbaldini <luca.baldini@pi.infn.it>
2018-12-03 14:37:26 +0100

    Prepare for tag 1.1.0.

--- __version__.h
+++ __version__.h
@@ -1,2 +1,2 @@
-#define __XPEDAQ_VERSION__ "1.0.0"
-#define __XPEDAQ_BUILD_DATE__ "Thu, 29 Nov 2018 16:18:26 +0200"
+#define __XPEDAQ_VERSION__ "1.1.0"
+#define __XPEDAQ_BUILD_DATE__ "Mon, 03 Dec 2018 14:37:24 +0100"
